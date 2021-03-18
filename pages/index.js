73d374b2b4c56b629040c9bd393b5506135252a9
jonatas-ms-student <jonatasmsstudent@gmail.com>
2021-03-18 14:29:02 -0300

    adicionando um sub título

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ const Home = () => {
   return (
     <div>
       <h1>Moramigo</h1>
+      <br />
+      <h3>Encontrar uma galera bacana para compartilhar moradia não precisa ser uma tarefa difícil.</h3>
     </div>
   );
 }
